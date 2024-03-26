c18aa9951b83d5924f5767ede2f556b5d0e57200
alexsavinov <spell477@gmail.com>
2024-03-26 16:48:18 +0200

    1st commit

--- src/main/java/com/itermit/springtest02/model/entity/RefreshToken.java
+++ src/main/java/com/itermit/springtest02/model/entity/RefreshToken.java
@@ -24,7 +24,7 @@ public class RefreshToken {
     @Column(nullable = false)
     private Instant expiryDate;
 
-    @OneToOne(fetch = FetchType.EAGER)
+    @OneToOne(fetch = FetchType.EAGER, orphanRemoval = true)
     @JoinColumn(name = "usr_id", referencedColumnName = "id")
     private User user;
 }
